ea06851c518ea7154984dd64690519ee33e32862
Willy Wonka <Woodenehad9798740@yandex.ru>
2023-01-12 17:08:39 +0300

    config Postgre DB

--- app/config.py
+++ app/config.py
@@ -1,14 +1,21 @@
 import os
 from dotenv import load_dotenv
 
-base_dir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(base_dir, ".env"))
+project_path = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(project_path, ".env"))
 
 
-class Config:
-    db_dir = "sqlite:///" + os.path.join(base_dir, "app.db")
-
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or db_dir
-    # уведомления о необходимости внесения изменений в БД
+class ConfigPostgres:
+    POSTGRES_URI = "postgresql://:5432/flask_db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or POSTGRES_URI
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ConfigSqlite:
+    SQLITE_URI = "sqlite:///" + os.path.join(project_path, "app.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or SQLITE_URI
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class Config(ConfigPostgres):
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
