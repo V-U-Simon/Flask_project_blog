d2f9396fa310d9f7386f52e62615a126e1a051f1
Willy Wonka <Woodenehad9798740@yandex.ru>
2023-01-12 17:10:56 +0300

    config flask-migrate

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ from app.config import Config
 from app import commands
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
 login = LoginManager()
 bootstrap = Bootstrap()
+migrate = Migrate()
 
 
 def create_app() -> Flask:
@@ -24,6 +26,7 @@ def create_app() -> Flask:
 
 def register_extensions(app: Flask):
     db.init_app(app)
+    migrate.init_app(app, db)
     login.init_app(app)
     login.login_view = 'user.login'
     bootstrap.init_app(app)
@@ -36,12 +39,12 @@ def register_commands(app: Flask):
 
 def register_blueprints(app: Flask):
     # fmt: off
+    from app.main.views import bp as main_bp
+    app.register_blueprint(main_bp)
+    
     from app.user import bp as user_bp
     app.register_blueprint(user_bp, url_prefix="/user")
 
     from app.article import bp as article_bp
     app.register_blueprint(article_bp, url_prefix="/article")
     # fmt: on
-
-
-    
