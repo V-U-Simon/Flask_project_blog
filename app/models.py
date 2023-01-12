eaa78ee96dedc3ebc4fd8a345f318d0f54ec8516
Willy Wonka <Woodenehad9798740@yandex.ru>
2023-01-13 01:06:52 +0300

    add magrations init & users with admin

--- app/models.py
+++ app/models.py
@@ -6,11 +6,12 @@ from datetime import datetime
 
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'users'
+    __tablename__ = 'flask_users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    is_admin = db.Column(db.Boolean(), default=False)
     password_hash = db.Column(db.String(128))
     # posts = db.relationship('Article', backref='author', lazy='dynamic')
     
