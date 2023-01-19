21e69141fc4e6f0342ce2f56c6221f4c6328a472
Willy Wonka <Woodenehad9798740@yandex.ru>
2023-01-19 13:19:33 +0300

    add Article model

--- app/models.py
+++ app/models.py
@@ -4,39 +4,60 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 
-
 class User(db.Model, UserMixin):
-    __tablename__ = 'flask_users'
+    __tablename__ = "user_table"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    is_admin = db.Column(db.Boolean(), default=False)
+    is_staff = db.Column(db.Boolean(), default=False)
     password_hash = db.Column(db.String(128))
-    # posts = db.relationship('Article', backref='author', lazy='dynamic')
-    
+
+    author = db.relationship("Author", backref="user", uselist=False)
+
     def __str__(self):
-        return f'{self.username}'
-    
+        return f"<User {self.username}>"
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
     
-    
+    @property
+    def is_author(self) -> bool:
+        return bool(self.author)
+        
+
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
-# waiting in the wings ;)
-# class Article(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(140))
-#     body = db.Column(db.String(256))
-#     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-#     language = db.Column(db.String(5))
 
-#     def __repr__(self):
-#         return f'<Article {self.title}>'
+class Author(db.Model):
+    __tablename__ = "author_table"
+    id = db.Column(db.Integer, primary_key=True)
+    
+    user_id = db.Column(db.Integer, db.ForeignKey("user_table.id"), nullable=True)
+    articles = db.relationship("Article", backref="author")
+
+    def __repr__(self):
+        return f"<Author: {self.user}>"
+
+
+class Article(db.Model):
+    __tablename__ = "article_table"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_updated = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+
+    author_id = db.Column(db.Integer, db.ForeignKey("author_table.id"), nullable=True)
+
+    def __repr__(self):
+        return f"<Article {self.title}>"
