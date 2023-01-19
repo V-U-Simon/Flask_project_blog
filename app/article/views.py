791a976eb41869f38d67d1de18d17a4185c9ca70
Willy Wonka <Woodenehad9798740@yandex.ru>
2023-01-19 15:08:56 +0300

    add Article (views & templates)

--- app/article/views.py
+++ app/article/views.py
@@ -1,49 +1,58 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from app.models import Article, db
 from app.article import bp
 
 
-ARTICLES = {
-    1: {
-        "title": "Sample article post",
-        "text": "This blog post shows a few different types of content that’s supported and styled with Bootstrap. Basic typography, lists, tables, images, code, and more are all supported as expected.",
-        "author": {
-            "name": "Alice",
-            "id": 1,
-        },
-    },
-}
-
-
-@bp.route("")
+@bp.route("/articles/")
 def list():
-    return render_template("article/list.html", articles=ARTICLES)
+    articles = Article.query.all()
+    return render_template("article/list.html", articles=articles)
 
 
-bp.route("/by_user/<int:user_pk>")
-def list_filter_by_user(user_pk):
-    return render_template("article/list.html", articles=ARTICLES)
+@bp.route("/articles/<int:id>/")
+def detail(id):
+    article = Article.query.get(id)
+    return render_template("article/detail.html", article=article)
 
 
-@bp.route("/create", methods=["GET", "POST"])
+@bp.route("/articles/create/", methods=["GET", "POST"])
 def create():
-    return render_template("article/create.html")
+    # todo: добавить форму из WTForm | form = ArticleForm
+    if request.method == "POST":
+        title = request.form.get("title")
+        body = request.form.get("body")
+        author_id = request.form.get("author_id")
+
+        article = Article(title=title, body=body, author_id=author_id)
+        db.session.add(article)
+        db.session.commit()
+
+        return redirect(url_for(".list"))
+    else:
+        # return render_template("article/form.html", form=form)
+        return render_template("article/form.html")
 
 
-@bp.route("/<int:pk>")
-def detail(pk: int):
-    return render_template("article/detail.html", article=ARTICLES[pk])
+@bp.route("/articles/<int:id>/update/", methods=["GET", "POST"])
+def update(id):
+    article = Article.query.get(id)
+    if request.method == "POST":
+        article.title = request.form.get("title")
+        article.body = request.form.get("body")
+        article.author_id = request.form.get("author_id")
+
+        db.session.commit()
+        return redirect(url_for(".detail", id=id))
+    else:
+        return render_template("article/form.html", article=article)
 
 
-@bp.route("/<int:pk>/update", methods=["GET", "POST"])
-def update(pk: int):
-    return render_template("article/update.html")
-
-
-@bp.route("/<int:pk>/delete", methods=["GET", "POST"])
-def delete(pk: int):
-    return render_template("article/delete.html")
-
-
-@bp.route("/<int:pk>/confirme_delete", methods=["GET", "POST"])
-def confirme_delete(pk: int):
-    return render_template("article/confirme_delete.html")
+@bp.route("/articles/<int:id>/delete/", methods=["GET", "POST"])
+def delete(id):
+    article = Article.query.get(id)
+    if request.method == "POST":
+        db.session.delete(article)
+        db.session.commit()
+        return redirect(url_for(".list"))
+        # return render_template("deleted_successfully.html")
+    return render_template("confirme_delete.html", object=article)
