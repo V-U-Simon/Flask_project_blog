c75247a0dabf5c4271c2649f73a38b2b3a7ce053
Willy Wonka <Woodenehad9798740@yandex.ru>
2023-02-03 11:36:30 +0300

    APP_Flask-Admin: one example working

--- app/__init__.py
+++ app/__init__.py
@@ -9,42 +9,23 @@ from flask_bootstrap import Bootstrap
 from flask_admin import Admin
 
 
-
 db = SQLAlchemy()
 login = LoginManager()
 bootstrap = Bootstrap()
 migrate = Migrate()
-admin = Admin(name="Article Admin Panel", template_mode="bootstrap4")
 
 
 def create_app(config_class=Config) -> Flask:
+    from app import models
+
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     register_extensions(app)
     register_commands(app)
     register_blueprints(app)
+    register_admins(app, models)
 
-    from app import models
-    
-    
-    from flask_admin.contrib.sqla import ModelView
-    
-    class CustomAdminView(ModelView):
-
-        def create_blueprint(self, admin):
-            blueprint = super().create_blueprint(admin)
-            blueprint.name = f'{blueprint.name}_admin'
-            return blueprint
-        
-        
-        def get_url(self, endpoint, **kwargs):
-            if not (endpoint.startswith('.') or endpoint.startswith('admin.')):
-                endpoint = endpoint.replace('.', '_admin.')
-            return super().get_url(endpoint, **kwargs)
-    
-    admin.add_view(CustomAdminView(models.Article, db.session, category='Models'))
-    
     return app
 
 
@@ -54,7 +35,6 @@ def register_extensions(app: Flask):
     login.init_app(app)
     login.login_view = "user.login"
     bootstrap.init_app(app)
-    admin.init_app(app)
 
 
 def register_commands(app: Flask):
@@ -71,10 +51,29 @@ def register_blueprints(app: Flask):
 
     from app.article import bp as article_bp
     app.register_blueprint(article_bp, url_prefix="/article")
-    
-    
-    # from app.admin import register_views
-    # register_views()
-    # from app.admin import bp as admin_bp
-    # app.register_blueprint(admin_bp, url_prefix="/admin")
     # fmt: on
+
+
+def register_admins(app, models):
+    admin = Admin(name="Article Admin Panel", template_mode="bootstrap4")
+    admin.init_app(app)
+    
+    from app.admin.views import CustomAdminView
+    admin.add_view(CustomAdminView(models.Article, db.session, category="Models"))
+    
+    return admin
+
+    # from flask_admin.contrib.sqla import ModelView
+    # class CustomAdminView(ModelView):
+
+    #     def create_blueprint(self, admin):
+    #         blueprint = super().create_blueprint(admin)
+    #         blueprint.name = f'{blueprint.name}_admin'
+    #         return blueprint
+
+    #     def get_url(self, endpoint, **kwargs):
+    #         if not (endpoint.startswith('.') or endpoint.startswith('admin.')):
+    #             endpoint = endpoint.replace('.', '_admin.')
+    #         return super().get_url(endpoint, **kwargs)
+
+    ...
--- app/admin/views.py
+++ app/admin/views.py
@@ -1,20 +1,22 @@
-# from flask_admin.contrib.sqla import ModelView
+from flask_admin.contrib.sqla import ModelView
 # from flask_login import current_user
 # from flask import redirect, url_for
 
 
-# class CustomAdminView(ModelView):
+class CustomAdminView(ModelView):
+    # def __init__(self, model, session, name=None, category=None, endpoint=None, url=None, static_folder=None, menu_class_name=None, menu_icon_type=None, menu_icon_value=None):
+    #     super().__init__(model, session, name, category, endpoint, url, static_folder, menu_class_name, menu_icon_type, menu_icon_value)
 
-#     def create_blueprint(self, admin):
-#         blueprint = super().create_blueprint(admin)
-#         blueprint.name = f'{blueprint.name}_admin'
-#         return blueprint
+    def create_blueprint(self, admin):
+        blueprint = super().create_blueprint(admin)
+        blueprint.name = f'{blueprint.name}_admin'
+        return blueprint
     
     
-#     def get_url(self, endpoint, **kwargs):
-#         if not (endpoint.startswith('.') or endpoint.startswith('admin.')):
-#             endpoint = endpoint.replace('.', '_admin.')
-#         return super().get_url(endpoint, **kwargs)
+    def get_url(self, endpoint, **kwargs):
+        if not (endpoint.startswith('.') or endpoint.startswith('admin.')):
+            endpoint = endpoint.replace('.', '_admin.')
+        return super().get_url(endpoint, **kwargs)
 
     
 
